098de420d206e11470ffd950d78032b934b98719
Aiko Fujimoto <aikoo_fujimotoo@yahoo.co.jp>
2020-11-05 20:24:54 +0900

    [chore] Update version
    Is there a way to programmatically check version defined in pyproject.toml?
    Dunno and I want to change this.

--- heybrochecklog/__init__.py
+++ heybrochecklog/__init__.py
@@ -38,7 +38,7 @@ def parse_args():
         '--version',
         help='Print version info.',
         action='version',
-        version='1.2.1-doujinmusicdiscord'
+        version='1.3.2-doujinmusicdiscord'
     )
 
     return parser.parse_args()
